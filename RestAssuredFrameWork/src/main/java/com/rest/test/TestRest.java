1e97454916ec4167df2a9f632cb2fdb8e77154bf
Veeresha Badappali. Palakshappa <vbpalaks@oradev.oraclecorp.com>
2019-11-23 23:58:57 +0530

    My first Commit

---
+++ RestAssuredFrameWork/src/main/java/com/rest/test/TestRest.java
@@ -0,0 +1,13 @@
+package com.rest.test;
+
+import org.testng.annotations.Test;
+
+public class TestRest {
+	
+	@Test
+	public void Myfirst() {
+		// TODO Auto-generated method stub
+    System.out.println("Welcome");
+	}
+
+}
